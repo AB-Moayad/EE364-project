b5a01bb1386160f6380ef3af46785f226e1371e3
AB-Moayad <134235946+AB-Moayad@users.noreply.github.com>
2023-05-22 21:57:15 +0300

    Pathway.java

--- Pathway.java
+++ Pathway.java
@@ -7,12 +7,20 @@ public class Pathway {
     private Organizer[] organizers;
     private Direction direction;
     private int floor;
-  
+    private String pathwayName;
 
-    public Pathway(){
-
+    public Pathway(String pathwayName) {
+        setPathwayName(pathwayName);
     }
-    
+
+    public void setPathwayName(String pathwayName) {
+        this.pathwayName = pathwayName;
+    }
+
+    public String getPathwayName() {
+        return pathwayName;
+    }
+
     public double getLength() {
         return length;
     }
@@ -27,13 +35,13 @@ public class Pathway {
 
     public void setCapacity(int capacity) {
         this.capacity = 100;
-        if (capacity >=100){
+        if (capacity >= 100) {
             System.out.println("The place is full");
         }
 
-        else{
+        else {
             int room = 0;
-            int space = 100/room;
+            int space = 100 / room;
             System.out.println("The place has:" + space + "capacity out of 50");
         }
     }
@@ -84,5 +92,10 @@ public class Pathway {
 
     public void setFloor(int floor) {
         this.floor = floor;
+    }
+
+    @Override
+    public String toString() {
+        return  getPathwayName();
     }
 }
