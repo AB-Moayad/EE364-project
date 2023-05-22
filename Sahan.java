0abfc56d33b226b8d53a31f29f5030b6dbc6d9f9
AB-Moayad <134235946+AB-Moayad@users.noreply.github.com>
2023-05-22 21:58:50 +0300

    Sahan.java

--- Sahan.java
+++ Sahan.java
@@ -1,12 +1,21 @@
 public class Sahan extends Pathway {
     private int stage;
-
+    
+    public Sahan(){
+        super("Sahan");
+    }
 
     public int getStage() {
+        for (int visitorStage = 0;visitorStage < 7;visitorStage++){
+            System.out.println("The visitor is in" + visitorStage + "Stage");
+            ++stage;
+        }
+
         return stage;
     }
 
     public void selectFloor(Visitor visitor, int floor) {
-        // assign floor to visitor
-    }
+        if (visitor.getDirection() == 1){
+            getCapacity();
+        }    }
 }
