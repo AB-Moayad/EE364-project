c05f709db9747d1c2bca6d050b1ce60944fd8604
AB-Moayad <134235946+AB-Moayad@users.noreply.github.com>
2023-05-27 13:20:27 +0300

    Update PrayLocation.java

--- PrayLocation.java
+++ PrayLocation.java
@@ -5,13 +5,12 @@ public class PrayLocation extends HaramTarget {
     private int Capacity;
     private int Floor;
     private boolean Inside;
-    private ArrayList<String> Name = new ArrayList<String>();
     private int Time_of_prayer;
     private double PrayerTime;
+    private Pathway pathway;
     int seats = 0;
-    
-    Random random = new Random();
 
+    Random random = new Random();
 
     public PrayLocation(String mousqe_name) {
         super(mousqe_name);
@@ -21,24 +20,26 @@ public class PrayLocation extends HaramTarget {
         return Capacity;
     }
 
-    public void setCapacity(ArrayList<String> name ) {
-        
+    public void setCapacity(ArrayList<String> name) {
+
         this.Capacity = 50;
-        if (Capacity >=50){
+        if (Capacity >= 50) {
             System.out.println("The place is full");
         }
 
-        else{
-            int space = 50/seats;
+        else {
+            int space = 50 / seats;
             System.out.println("The place has:" + space + "capacity out of 50");
         }
-        
+
     }
-// when we do phase 2 
+
+    // when we do phase 2
     public int getFloor() {
         return Floor;
     }
-// when we do phase 2 
+
+    // when we do phase 2
     public void setFloor(int floor) {
         this.Floor = floor;
     }
@@ -57,18 +58,6 @@ public class PrayLocation extends HaramTarget {
         return Inside;
     }
 
-    public String getName() {
-        int randomIndex = random.nextInt(Name.size());
-        return Name.get(randomIndex);
-    }
-
-    public void setName(ArrayList<String> name) {
-        for (int i = 1;i<8;i++){
-            Name.add("Pray Location #"+i);
-            System.out.println(Name.get(i-1));
-        }    
-    }
-
     public int getTime_of_prayer() {
         return Time_of_prayer;
     }
@@ -77,20 +66,20 @@ public class PrayLocation extends HaramTarget {
 
         switch (PrayerName) {
             case "Fajir":
-                this.Time_of_prayer =  random.nextInt(10,21);
+                this.Time_of_prayer = random.nextInt(10, 21);
             case "Duhr":
             case "Asur":
-                this.Time_of_prayer =  random.nextInt(15,21);
+                this.Time_of_prayer = random.nextInt(15, 21);
             case "Magreeb":
-                this.Time_of_prayer =  random.nextInt(10,21);
+                this.Time_of_prayer = random.nextInt(10, 21);
             case "Ashia":
-                this.Time_of_prayer =  random.nextInt(15,25);
+                this.Time_of_prayer = random.nextInt(15, 25);
             default:
-            this.Time_of_prayer =  random.nextInt(10,20);
+                this.Time_of_prayer = random.nextInt(10, 20);
         }
     }
 
-    //need JavaFX
+    // need JavaFX
     public double getPrayerTime() {
         return PrayerTime;
     }
@@ -99,4 +88,16 @@ public class PrayLocation extends HaramTarget {
         PrayerTime = prayerTime;
     }
 
+    public void setPathway(Pathway pathway) {
+        this.pathway = pathway;
+    }
+
+    public Pathway getPathway() {
+        return pathway;
+    }
+
+    @Override
+    public String toString() {
+        return getTargetName();
+    }
 }
